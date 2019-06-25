5d40128fb0d8d50f478183dcee3b4d2d7e2e34e5
nikostourvas <nikostourvas@gmail.com>
2019-06-25 14:10:40 +0000

    update function to return multiple arguments

--- analysis/barplot_N_alleles.R
+++ analysis/barplot_N_alleles.R
@@ -5,16 +5,19 @@ all <- as.data.frame(div$pop.n.all)
 all <- all %>% 
         mutate(population = rownames(all)) %>% 
         rename(count = "div$pop.n.all")
+all$maf <- maf
 
 all$count <- as.integer(all$count)
 
 # str(all)
 
-ggplot(all, aes(x=population, y=count) ) +
+p <- ggplot(all, aes(x=population, y=count) ) +
         geom_bar(stat = "identity", 
                  color = "black", fill = "white",
                  width = .8) +
         geom_text(aes(label=count, vjust = 1.6)) +
         ggtitle("Total Number of alleles per population")
 
+
+all_list <- list("data" = all, "plot" = p)
 }
